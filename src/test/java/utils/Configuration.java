afce0b226a45ce6c1debbfb32f44674fcd49fc0e
Daniel Paz <Daniel.Paz@endava.com>
2019-09-06 17:26:31 -0500

    Fix properties path

--- src/test/java/utils/Configuration.java
+++ src/test/java/utils/Configuration.java
@@ -7,11 +7,12 @@ import java.util.Properties;
 
 public class Configuration {
 
+    private static final String CONFIG_PROPERTIES_PATH = "src/test/resources/config/config.properties";
     private static String property = null;
 
     public static String getProperties(String variable) {
         try {
-            InputStream fileInput = new FileInputStream("src/test/resources/config/config.properties");
+            InputStream fileInput = new FileInputStream(CONFIG_PROPERTIES_PATH);
             Properties properties = new Properties();
             properties.load(fileInput);
             property = properties.getProperty(variable);
